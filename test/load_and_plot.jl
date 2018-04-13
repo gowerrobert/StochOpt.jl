8e079495f22450b47444946bb1ee56c153e20515
Robert M. Gower <gowerrobert@gmail.com>
2018-04-13 15:46:06 +0200

    load and pliot

--- test/load_and_plot.jl
+++ test/load_and_plot.jl
@@ -12,13 +12,16 @@ options = set_options();
 datapath = ""#
 probnames = ["mushrooms"]#[   "phishing", "madelon",  "a9a",  "mushrooms", "w8a", "gisette_scale"  ,"covtype", "rcv1_train"]# mushrooms
 # probnames = ["phishing", "madelon",  "a9a",  "mushrooms", "phishing", "w8a", "gisette_scale"  ,"covtype"]#  rcv1_train  liver-disorders_scale
+options.batchsize = 100;
 for probname in probnames
   # probname = "madelon";
-  name = string("lgstc_",  probname);
-  # prob =  load_logistic(probname,datapath,options);
-  # boot_method("-", prob,options);
-  default_path = "./data/";   loadname= replace(name, r"[\/]", "-");
-  OUTPUTS = load("$(default_path)$(loadname).jld", "OUTPUTS");
+  # name = string("lgstc_",  probname);
+  # beststep, savename = get_saved_stepsize(prob.name, method_name,options);
+   prob =  load_logistic(probname,datapath,options);
+   boot_method("-", prob,options);
+   load_fsol!(options, prob);  # load a pre-calculated best  solution
+   default_path = "./data/";   loadname= replace(prob.name, r"[\/]", "-");
+   OUTPUTS = load("$(default_path)$(loadname).jld", "OUTPUTS");
 ##  Some code for editing which methods are plotted
      # OUTPUTNEW = OUTPUTS[1];
      # OUTPUTNEW = [OUTPUTNEW ; OUTPUTS[4:end]];
