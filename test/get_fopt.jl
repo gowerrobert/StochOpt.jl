6422f46e7fb3900c4dc605611cf304b832e56327
ngazagna <nidham.gazagnadou@gmail.com>
2018-08-02 16:14:42 +0200

    small

--- test/get_fopt.jl
+++ test/get_fopt.jl
@@ -17,27 +17,27 @@ include("../src/StochOpt.jl")
 # rep_number = 1;# number of times the optimization should be repeated.
 # options = MyOptions(tol,Inf,maxiter,skip_error_calculation,max_time,max_epocs,
 # printiters,exacterror,0,"normalized",0.0,false, force_continue,rep_number,0)
-options = set_options(tol =10.0^(-16.0), skip_error_calculation =20, exacterror =false, max_iter=10^8, max_time =60.0*60.0*3.0,
- max_epocs = 500, repeat_stepsize_calculation = true, rep_number =2);
+options = set_options(tol = 10.0^(-16.0), skip_error_calculation = 20, exacterror = false, max_iter = 10^8, max_time = 60.0*60.0*3.0,
+                      max_epocs = 500, repeat_stepsize_calculation = true, rep_number = 2);
 
-datapath = ""# "/local/rgower/libsvmdata/"
+datapath = "" # "/local/rgower/libsvmdata/"
 default_path = "./data/";
-#problemnames =[ "mushrooms"] #,
-problemnames =[  "a9a"] #,  [  "a9a", "madelon", "phishing", "covtype", "gisette_scale", "w8a"]
+#problemnames =["mushrooms"] #,
+problemnames = ["a9a"] #,  ["a9a", "madelon", "phishing", "covtype", "gisette_scale", "w8a"]
 
- for probname in problemnames
-  prob =  load_logistic(probname,datapath,options);  # Loads logisitc
-  options.batchsize =prob.numdata; ## load problem
-  # prob.fsol = 0.0;
-  method_name = "BFGS";
-  output= minimizeFunc_grid_stepsize(prob, method_name, options);
-  options.embeddim =  [prob.numdata ,0.01];  #[0.9, 5];
-  method_name = "BFGS_accel";
-  output1= minimizeFunc_grid_stepsize(prob, method_name, options);
-  OUTPUTS = [output ; output1];
-  pgfplots()# gr() pyplot() # pgfplots() #plotly()
-  plot_outputs_Plots(OUTPUTS,prob,options)
-  fsol = minimum([output.fs output1.fs]);#min(output.fs[end],fsol);
-  fsolfilename = get_fsol_filename(prob);
-  save("$(fsolfilename).jld", "fsol", fsol)
- end
+for probname in problemnames
+    prob = load_logistic(probname, datapath, options);  # Loads logisitc
+    options.batchsize = prob.numdata; ## load problem
+    # prob.fsol = 0.0;
+    method_name = "BFGS";
+    output = minimizeFunc_grid_stepsize(prob, method_name, options);
+    options.embeddim = [prob.numdata, 0.01];  #[0.9, 5];
+    method_name = "BFGS_accel";
+    output1 = minimizeFunc_grid_stepsize(prob, method_name, options);
+    OUTPUTS = [output; output1];
+    gr()# gr() pyplot() # pgfplots() #plotly()
+    plot_outputs_Plots(OUTPUTS, prob,options)
+    fsol = minimum([output.fs output1.fs]);#min(output.fs[end],fsol);
+    fsolfilename = get_fsol_filename(prob);
+    save("$(fsolfilename).jld", "fsol", fsol)
+end
