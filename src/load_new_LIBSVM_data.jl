b256a5021ba500e76441eb5dcce3241b6532bb89
ngazagna <nidham.gazagnadou@gmail.com>
2018-08-03 17:53:25 +0200

    indentation

--- src/load_new_LIBSVM_data.jl
+++ src/load_new_LIBSVM_data.jl
@@ -2,9 +2,9 @@
 include("dataLoad.jl")
 initDetails()
 
-datasets = ["splice.t"] #  w1a, SUSY, pendigits, heart
-for  dataset in datasets
-transformDataJLD(dataset)
-X,y = loadDataset(dataset) #
-showDetails(dataset)
+datasets = ["covtype"] #  w1a, SUSY, pendigits, heart
+for dataset in datasets
+    transformDataJLD(dataset)
+    X,y = loadDataset(dataset) #
+    showDetails(dataset)
 end
