ac2482cec1a8a651498b624fb1c30cb492758c24
ngazagna <nidham.gazagnadou@gmail.com>
2018-08-06 11:06:25 +0200

    github test

--- repeat_paper_experiments/compare_SAGA_importance_opt_Lis.jl
+++ repeat_paper_experiments/compare_SAGA_importance_opt_Lis.jl
@@ -6,12 +6,12 @@ include("../src/StochOpt.jl")
 
 ## Basic parameters and options for solvers
 options = set_options(max_iter=10^8, max_time =10.0, max_epocs = 50, repeat_stepsize_calculation = true, regulatrizor_parameter="1/num_data", initial_point = "randn"); # =100, ,skip_error_calculation =5
-options.batchsize =1;
-options.stepsize_multiplier =1;
+options.batchsize = 1;
+options.stepsize_multiplier = 1;
 ## load data
 
 numdata = 2000;
-numfeatures =numdata;
+numfeatures = numdata;
 lambda_input = 1/(numdata^2);
 a =1./numdata;
   # X, y, probname = gen_diag_lone_eig_data(numfeatures, numdata, lambda = lambda_input, a =1/lambda_input );
