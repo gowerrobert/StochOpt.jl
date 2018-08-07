b66bbba7a843aeba86469eac317f76ba23ff5469
ngazagna <nidham.gazagnadou@gmail.com>
2018-08-07 14:54:25 +0200

    correcting space error

--- util/preprocessing.jl
+++ util/preprocessing.jl
@@ -7,7 +7,7 @@ function fit_apply_datascaling(X, scaling::AbstractString)
       stdX = std(X, 2);
       ind = (0. == stdX); stdX[ind]=1.0; #     #replace 0 in std by 1 incase there is a constant feature
       colsmean = mean(X, 2);
-      X[:] = (X. - colsmean);
+      X[:] = (X.- colsmean);
       X[:] = X./stdX; # Centering and scaling the data.
       colscaling = stdX.^(-1);
     elseif(scaling == "column-row-scaling")
