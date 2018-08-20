7187e93cb8eccd79444a2700a35a1cfaee4542e9
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2018-08-20 18:22:31 +0200

    loop over minibatch sizes

--- test/demo_SAGA_optimal_minibatch.jl
+++ test/demo_SAGA_optimal_minibatch.jl
@@ -9,31 +9,24 @@ options = set_options(max_iter=10^8, max_time=1000.0, max_epocs=150, force_conti
 
 ## Load problem
 datapath = ""
-probname = "splice";   # Data tested in paper: w8a mushrooms gisette_scale,  madelon  a9a  phishing  covtype splice  rcv1_train  liver-disorders_scale
+probname = "mushrooms"; # "splice"   # Data tested in paper: w8a mushrooms gisette_scale,  madelon  a9a  phishing  covtype splice  rcv1_train  liver-disorders_scale
 prob = load_logistic(probname, datapath, options);  # Loads logisitc problem
 # options.stepsize_multiplier = 10; # Why ? because it is set in the following function "initiate_SAGA"
 
 ## Running methods
 OUTPUTS = [];  # List of saved outputs
 
-###### SAGA + nice minibatch + default ("") probability
-## Minibatch size = 1 : SGD
-options.batchsize = 1;
-sg = initiate_SAGA(prob, options, minibatch_type="nice");
-output = minimizeFunc(prob, sg, options);
-OUTPUTS = [OUTPUTS; output];
+## List of minibatch sizes
+batchsizelist = [1, 2, 5, 10]; 
 
-## Minibatch size = 10
-options.batchsize = 10;
-sg = initiate_SAGA(prob, options, minibatch_type="nice");
-output = minimizeFunc(prob, sg, options);
-OUTPUTS = [OUTPUTS; output];
-
-## Minibatch size = 20
-options.batchsize = 20;
-sg = initiate_SAGA(prob, options, minibatch_type="nice");
-output = minimizeFunc(prob, sg, options);
-OUTPUTS = [OUTPUTS; output];
+###### SAGA + nice minibatching
+for tau in batchsizelist
+    println("\nCurrent minibatch size: ", tau);
+    options.batchsize = tau;
+    sg = initiate_SAGA(prob, options, minibatch_type="nice");
+    output = minimizeFunc(prob, sg, options);
+    OUTPUTS = [OUTPUTS; output];
+end
 
 ## Minibatch size = numdata : Gradrient descent
 # options.batchsize = prob.numdata;
