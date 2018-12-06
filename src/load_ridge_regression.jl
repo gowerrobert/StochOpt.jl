087d27b95be0bee9b7633bc4dbcc4f966e7de069
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2018-12-06 15:58:14 +0100

    declaration problem

--- src/load_ridge_regression.jl
+++ src/load_ridge_regression.jl
@@ -4,7 +4,7 @@ function load_ridge_regression(X, y::Array{Float64}, name::AbstractString, opts:
     #  f_i(w) =  1/2(X_{:i}^T w  -y_i)^2 + lambda/2 ||w||^2
     #  nabla f_i =  X_{:i}(X_{:i}^T w  -y_i)^2 + lambda w
 
-    name = string("ridge_", probname);
+    name = string("ridge_", name);
 
     ## standard normalization. Leave this for choosing X
     if(typeof(scaling) == String)
