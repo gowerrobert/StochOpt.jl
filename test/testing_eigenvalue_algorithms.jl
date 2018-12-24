10bea0661d5705bc15112b1c0e4e7d568d11be98
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2018-12-24 17:06:20 +0100

    finishing benchmark of eigmax and eigmin

--- test/testing_eigenvalue_algorithms.jl
+++ test/testing_eigenvalue_algorithms.jl
@@ -1,13 +1,12 @@
+using Random
 using Printf
 using LinearAlgebra # julia 0.7
 
-include("./util/power_iteration.jl") # Be carefull about the path here
+include("../util/power_iteration.jl") # Be carefull about the path here
 
-function gen_sym_matrix(d)
-    A = rand(d, d)
-    return A'*A
-end
+Random.seed!(1);
 
+## Display function for elapsed time
 function sec2time(t::Float64)
     (remainder, nanoseconds) = fldmod(t*1e9, 1000)
     (remainder, microseconds) = fldmod(remainder, 1000)
@@ -22,6 +21,20 @@ function sec2time(t::Float64)
     return minutes, seconds, milliseconds, microseconds, nanoseconds
 end
 
+
+function gen_sym_matrix(d)
+    A = rand(d, d)
+    return A'*A
+end
+
+## Symmetric version
+function gen_typed_sym_matrix(d)
+    A = rand(d, d)
+    return Symmetric(A'*A)
+end
+
+
+## Julia eigmax and eigmin
 function test_eigmax(d, numruns=10000)
     t = 0.0
     for it=1:numruns
@@ -30,7 +43,7 @@ function test_eigmax(d, numruns=10000)
     end
     t /= numruns
     m, s, ms, microsec, ns = sec2time(t)
-    @printf("eigmax average time          : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+    @printf("eigmax average time                      : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
 end
 
 function test_eigmin(d, numruns=10000)
@@ -41,9 +54,33 @@ function test_eigmin(d, numruns=10000)
     end
     t /= numruns
     m, s, ms, microsec, ns = sec2time(t)
-    @printf("eigmin average time           : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+    @printf("eigmin average time                      : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
 end
 
+## Symmetric versions
+function test_Symmetric_eigmax(d, numruns=10000)
+    t = 0.0
+    for it=1:numruns
+        A = gen_typed_sym_matrix(d)
+        t += @elapsed eigmax(A)
+    end
+    t /= numruns
+    m, s, ms, microsec, ns = sec2time(t)
+    @printf("Symmetric_eigmax average time            : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+end
+
+function test_Symmetric_eigmin(d, numruns=10000)
+    t = 0.0
+    for it=1:numruns
+        A = gen_typed_sym_matrix(d)
+        t += @elapsed eigmin(A)
+    end
+    t /= numruns
+    m, s, ms, microsec, ns = sec2time(t)
+    @printf("Symmetric_eigmin average time            : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+end
+
+## Power and inverse iteration
 function test_power_iteration(d, numruns=10000)
     t = 0.0
     for it=1:numruns
@@ -52,7 +89,7 @@ function test_power_iteration(d, numruns=10000)
     end
     t /= numruns
     m, s, ms, microsec, ns = sec2time(t)
-    @printf("power_iteration average time : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+    @printf("power_iteration average time             : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
 end
 
 function test_inverse_iteration(d, numruns=10000)
@@ -63,69 +100,248 @@ function test_inverse_iteration(d, numruns=10000)
     end
     t /= numruns
     m, s, ms, microsec, ns = sec2time(t)
-    @printf("inverse_iteration average time: %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+    @printf("inverse_iteration average time           : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
 end
+
+## Symmetric versions
+function test_Symmetric_power_iteration(d, numruns=10000)
+    t = 0.0
+    for it=1:numruns
+        A = gen_typed_sym_matrix(d)
+        t += @elapsed Symmetric_power_iteration(A)
+    end
+    t /= numruns
+    m, s, ms, microsec, ns = sec2time(t)
+    @printf("Symmetric_power_iteration average time   : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+end
+
+function test_Symmetric_inverse_iteration(d, numruns=10000)
+    t = 0.0
+    for it=1:numruns
+        A = gen_typed_sym_matrix(d)
+        t += @elapsed Symmetric_inverse_iteration(A)
+    end
+    t /= numruns
+    m, s, ms, microsec, ns = sec2time(t)
+    @printf("Symmetric_inverse_iteration average time : %02d m %02d s %03d ms %03d µs %03d ns (%d runs)\n", m, s, ms, microsec, ns, numruns)
+end
+
 
 
 ## --- Correctness of the approximation --- ##
 println("######## CORRECTNESS OF THE APPROXIMATION ########\n")
+numsim=10;
 A = gen_sym_matrix(100);
-println("Approximation error of the largest eigenvalue  : ", abs(eigmax(A)-power_iteration(A)))
-println("Approximation error of the smallest eigenvalue : ",abs(eigmin(A)-inverse_iteration(A)), "\n")
+B = gen_typed_sym_matrix(100);
+println("------ Largest eigenvalue approximation error ------")
+println("power_iteration              : ", abs(eigmax(A)-power_iteration(A, numsim=numsim)))
+println("Symmetric_power_iteration    : ", abs(eigmax(B)-Symmetric_power_iteration(B, numsim=numsim)), "\n")
+
+println("------ Smallest eigenvalue approximation error ------")
+println("inverse_iteration            : ", abs(eigmin(A)-inverse_iteration(A, numsim=numsim)))
+println("Symmetric_inverse_iteration  : ", abs(eigmin(B)-Symmetric_inverse_iteration(B, numsim=numsim)), "\n")
 
 
-## --- Time comparison --- ##
-println("######## TIME COMPARISON ########\n")
-println("------ eigmax vs power_iteration")
-# Dimension = 10
+
+# ## --- Time comparison --- ##
+# println("######## TIME COMPARISON ########\n")
+
+# ## --- Largest eigenvalue ---
+# println("------ eigmax vs Symmetric_eigmax vs power_iteration vs Symmetric_power_iteration ------")
+# # Dimension = 10
+# d = 10
+# numruns = 100000
+# println("d = ", d, ", number of runs = ", numruns)
+# test_eigmax(d, numruns)
+# test_Symmetric_eigmax(d, numruns)
+# test_power_iteration(d, numruns)
+# test_Symmetric_power_iteration(d, numruns)
+
+# # Dimension = 100
+# d = 100
+# numruns = 10000
+# println("\nd = ", d, ", number of runs = ", numruns)
+# test_eigmax(d, numruns)
+# test_Symmetric_eigmax(d, numruns)
+# test_power_iteration(d, numruns)
+# test_Symmetric_power_iteration(d, numruns)
+
+# # Dimension = 1000
+# d = 1000
+# numruns = 100
+# println("\nd = ", d, ", number of runs = ", numruns)
+# test_eigmax(d, numruns)
+# test_Symmetric_eigmax(d, numruns)
+# test_power_iteration(d, numruns)
+# test_Symmetric_power_iteration(d, numruns)
+
+# # Dimension = 10000
+# d = 10000
+# numruns = 1
+# println("\nd = ", d, ", number of runs = ", numruns)
+# test_eigmax(d, numruns)
+# test_Symmetric_eigmax(d, numruns)
+# test_power_iteration(d, numruns)
+# test_Symmetric_power_iteration(d, numruns)
+
+
+
+# ## --- Smallest eigenvalue ---
+# println("\n\n------ eigmin vs Symmetric_eigmin vs inverse_iteration vs Symmetric_inverse_iteration ")
+# # Dimension = 10
+# d = 10
+# numruns = 100000
+# println("d = ", d, ", number of runs = ", numruns)
+# test_eigmin(d, numruns)
+# test_Symmetric_eigmin(d, numruns)
+# test_inverse_iteration(d, numruns)
+# test_Symmetric_inverse_iteration(d, numruns)
+
+# # Dimension = 100
+# d = 100
+# numruns = 10000
+# println("\nd = ", d, ", number of runs = ", numruns)
+# test_eigmin(d, numruns)
+# test_Symmetric_eigmin(d, numruns)
+# test_inverse_iteration(d, numruns)
+# test_Symmetric_inverse_iteration(d, numruns)
+
+# # Dimension = 1000
+# d = 1000
+# numruns = 100
+# println("\nd = ", d, ", number of runs = ", numruns)
+# test_eigmin(d, numruns)
+# test_Symmetric_eigmin(d, numruns)
+# test_inverse_iteration(d, numruns)
+# test_Symmetric_inverse_iteration(d, numruns)
+
+# # Dimension = 10000
+# d = 10000
+# numruns = 1
+# println("\nd = ", d, ", number of runs = ", numruns)
+# test_eigmin(d, numruns)
+# test_Symmetric_eigmin(d, numruns)
+# test_inverse_iteration(d, numruns)
+# test_Symmetric_inverse_iteration(d, numruns)
+
+
+##################################################################################
+
+################ ESTIMATING COMPUTATION TIME ON THE SAME MATRICES ################
+function test_estimate_largest_eigval(d, numruns)
+    t1 = 0.0 # t_eigmax
+    t2 = 0.0 # t_Symmetric_eigmax
+    t3 = 0.0 # t_power_iteration
+    t4 = 0.0 # t_Symmetric_power_iteration
+    for it=1:numruns
+        # type(A) = Array{Float64,2}
+        A = gen_sym_matrix(d)
+        t1 += @elapsed eigmax(A)
+        t3 += @elapsed power_iteration(A)
+        # type(A) = Symmetric{Float64,Array{Float64,2}}
+        A = Symmetric(A)
+        t2 += @elapsed eigmax(A)
+        t4 += @elapsed Symmetric_power_iteration(A)
+    end
+    t1 /= numruns
+    t2 /= numruns
+    t3 /= numruns
+    t4 /= numruns
+    m1, s1, ms1, microsec1, ns1 = sec2time(t1)
+    m2, s2, ms2, microsec2, ns2 = sec2time(t2)
+    m3, s3, ms3, microsec3, ns3 = sec2time(t3)
+    m4, s4, ms4, microsec4, ns4 = sec2time(t4)
+
+    @printf("eigmax average time                        : %02d m %02d s %03d ms %03d µs %03d ns\n", m1, s1, ms1, microsec1, ns1)
+    @printf("Symmetric_eigmax average time              : %02d m %02d s %03d ms %03d µs %03d ns\n", m2, s2, ms2, microsec2, ns2)
+    @printf("power_iteration average time               : %02d m %02d s %03d ms %03d µs %03d ns\n", m3, s3, ms3, microsec3, ns3)
+    @printf("Symmetric_power_iteration average time     : %02d m %02d s %03d ms %03d µs %03d ns\n\n", m4, s4, ms4, microsec4, ns4)
+end
+
+function test_estimate_smallest_eigval(d, numruns)
+    t1 = 0.0 # t_eigmax
+    t2 = 0.0 # t_Symmetric_eigmax
+    t3 = 0.0 # t_power_iteration
+    t4 = 0.0 # t_Symmetric_power_iteration
+    for it=1:numruns
+        # type(A) = Array{Float64,2}
+        A = gen_sym_matrix(d)
+        t1 += @elapsed eigmin(A)
+        t3 += @elapsed inverse_iteration(A)
+        # type(A) = Symmetric{Float64,Array{Float64,2}}
+        A = Symmetric(A)
+        t2 += @elapsed eigmin(A)
+        t4 += @elapsed Symmetric_inverse_iteration(A)
+    end
+    t1 /= numruns
+    t2 /= numruns
+    t3 /= numruns
+    t4 /= numruns
+    m1, s1, ms1, microsec1, ns1 = sec2time(t1)
+    m2, s2, ms2, microsec2, ns2 = sec2time(t2)
+    m3, s3, ms3, microsec3, ns3 = sec2time(t3)
+    m4, s4, ms4, microsec4, ns4 = sec2time(t4)
+
+    @printf("eigmin average time                        : %02d m %02d s %03d ms %03d µs %03d ns\n", m1, s1, ms1, microsec1, ns1)
+    @printf("Symmetric_eigmin average time              : %02d m %02d s %03d ms %03d µs %03d ns\n", m2, s2, ms2, microsec2, ns2)
+    @printf("inverse_iteration average time             : %02d m %02d s %03d ms %03d µs %03d ns\n", m3, s3, ms3, microsec3, ns3)
+    @printf("Symmetric_inverse_iteration average time   : %02d m %02d s %03d ms %03d µs %03d ns\n\n", m4, s4, ms4, microsec4, ns4)
+end
+
+
+## --- Largest eigenvalue ---
+println("------ eigmax vs Symmetric_eigmax vs power_iteration vs Symmetric_power_iteration ------")
+
 d = 10
 numruns = 100000
-println("d = ", d, ", number of runs = ", numruns)
-test_eigmax(d, numruns)
-test_power_iteration(d, numruns)
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_largest_eigval(d, numruns)
 
-# Dimension = 100
 d = 100
-numruns = 1000
-println("\nd = ", d, ", number of runs = ", numruns)
-test_eigmax(d, numruns)
-test_power_iteration(d, numruns)
+numruns = 10000
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_largest_eigval(d, numruns)
 
-# Dimension = 10000
+d = 1000
+numruns = 100
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_largest_eigval(d, numruns)
+
+d = 2000
+numruns = 100
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_largest_eigval(d, numruns)
+
 d = 10000
-numruns = 1
-println("\nd = ", d, ", number of runs = ", numruns)
-test_eigmax(d, numruns)
-test_power_iteration(d, numruns)
+numruns = 10
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_largest_eigval(d, numruns)
 
-println("\n\n------ eigmin vs inverse_iteration")
-# Dimension = 10
+
+## --- Smallest eigenvalue ---
+println("------ eigmin vs Symmetric_eigmin vs inverse_iteration vs Symmetric_inverse_iteration ------")
+
 d = 10
 numruns = 100000
-println("d = ", d, ", number of runs = ", numruns)
-test_eigmin(d, numruns)
-test_inverse_iteration(d, numruns)
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_smallest_eigval(d, numruns)
 
-# Dimension = 100
 d = 100
-numruns = 1000
-println("\nd = ", d, ", number of runs = ", numruns)
-test_eigmin(d, numruns)
-test_inverse_iteration(d, numruns)
+numruns = 10000
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_smallest_eigval(d, numruns)
 
-# Dimension = 10000
+d = 1000
+numruns = 100
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_smallest_eigval(d, numruns)
+
+d = 2000
+numruns = 100
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_smallest_eigval(d, numruns)
+
 d = 10000
-numruns = 1
-println("\nd = ", d, ", number of runs = ", numruns)
-test_eigmin(d, numruns)
-test_inverse_iteration(d, numruns)
-
-## TEST SYMMETRIC
-# need to explore a bit more the effect of giving a Symmetric matrix as input...
-
-A = gen_sym_matrix(20);
-typeof(A)
-B = Symmetric(A);
-typeof(B) #Symmetric{Float64,Array{Float64,2}}
-@elapsed eigmin(A)
-@elapsed eigmin(B)
+numruns = 10
+@printf("Dimension = %d, number of runs %d\n", d, numruns)
+test_estimate_smallest_eigval(d, numruns)
--- util/power_iteration.jl
+++ util/power_iteration.jl
@@ -12,7 +12,6 @@ Computes the approximation largest eigenvalue of a matrix using the power iterat
     - **Float64** maxeig: approximation of the largest eigenvalue\\
 """
 function power_iteration(A::Array{Float64,2} ; b::Array{Float64}=rand(size(A,1),1), numsim::Int64=10)
-# function power_iteration(A::Array{Float64} ; b::Array{Float64}=rand(size(A,1),1), numsim::Int64=10)
     for i = 1:numsim
         b = A*b;
         b /= norm(b);
@@ -35,6 +34,52 @@ Computes an approximation of the smallest eigenvalue of a matrix using the power
     - **Float64** mineig: approximation of the smallest eigenvalue\\
 """
 function inverse_iteration(A::Array{Float64,2} ; b::Array{Float64}=rand(size(A,1),1), numsim::Int64=10)
+    for i = 1:numsim
+        b = A \ b;
+        b /= norm(b);
+    end
+    # mineig = (b' * (A \ b)) / (b'*b);
+    # mineig = 1/mineig[1]; # 1x1 Array{Float64,2} to Float64
+    mineig = (b'*b) / (b' * (A \ b));
+    return mineig[1]
+end
+
+"""
+    Symmetric_power_iteration(A ; b, numsim)
+
+Computes the approximation largest eigenvalue of a symmetric matrix using the power iteration algorithm.
+# Ref: "https://en.wikipedia.org/wiki/Power_iteration"
+
+#INPUTS:\\
+    - **Symmetric{Float64,Array{Float64,2}}** A: squared symmetric matrix for which we want the largest eigenvalue\\
+    - **Array{Float64}** b: initial vector\\
+    - **Int64** numsim: number of simulations (10 seems enough for small matrices)\\
+#OUTPUTS:\\
+    - **Float64** maxeig: approximation of the largest eigenvalue\\
+"""
+function Symmetric_power_iteration(A::Symmetric{Float64,Array{Float64,2}} ; b::Array{Float64}=rand(size(A,1),1), numsim::Int64=10)
+    for i = 1:numsim
+        b = A*b;
+        b /= norm(b);
+    end
+    maxeig = (b'*A*b)/(b'*b);
+    return maxeig[1] # 1x1 Array{Float64,2} to Float64
+end
+
+"""
+    Symmetric_inverse_iteration(A ; b, numsim)
+
+Computes an approximation of the smallest eigenvalue of a symmetric matrix using the power iteration algorithm.
+# Ref: "https://en.wikipedia.org/wiki/Inverse_iteration"
+
+#INPUTS:\\
+    - **Symmetric{Float64,Array{Float64,2}}** A: squared symmetric matrix for which we want the largest eigenvalue\\
+    - **Array{Float64}** b: initial vector\\
+    - **Int64** numsim: number of simulations (10 seems enough for small matrices)\\
+#OUTPUTS:\\
+    - **Float64** mineig: approximation of the smallest eigenvalue\\
+"""
+function Symmetric_inverse_iteration(A::Symmetric{Float64,Array{Float64,2}} ; b::Array{Float64}=rand(size(A,1),1), numsim::Int64=10)
     for i = 1:numsim
         b = A \ b;
         b /= norm(b);
