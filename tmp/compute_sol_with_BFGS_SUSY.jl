e99f60befdc75db7d921d46c3d4207d48cfe7ed0
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2019-01-02 10:55:12 +0100

    compute solution SUSY column scaling

--- tmp/compute_sol_with_BFGS_SUSY.jl
+++ tmp/compute_sol_with_BFGS_SUSY.jl
@@ -9,7 +9,7 @@ using LinearAlgebra # julia 0.7
 using Statistics # julia 0.7
 using Base64 # julia 0.7
 
-include("../src/StochOpt.jl") # Be carefull about the path here
+include("./src/StochOpt.jl") # Be carefull about the path here
 
 default_path = "./data/";
 
@@ -34,7 +34,7 @@ options = set_options(tol=10.0^(-1), max_iter=10^8, max_time=10.0^2, max_epocs=1
                       initial_point="zeros", # is fixed not to add more randomness 
                       force_continue=false); # force continue if diverging or if tolerance reached
 
-@time prob = load_logistic_from_matrices(X, y, data, options, lambda=-1, scaling="none");
+@time prob = load_logistic_from_matrices(X, y, data, options, lambda=-1, scaling="column-scaling");
 
 X = nothing; # available in prob.X
 y = nothing; # available in prob.y
@@ -43,33 +43,39 @@ y = nothing; # available in prob.y
 #region
 ## Computing the solution with a serial gridsearch
 @time get_fsol_logistic!(prob)
-## ---> First gridsearch on SUSY of ... hour led us to fsol = ...
-## (for ... , beststep = ... and batchsize = ...)
+## ---> First gridsearch on SUSY-none of 2 hours led us to fsol = 4.530221721802407e-6
+## (for BFGS, beststep = 2^5.0 and batchsize = prob.numdata)
+## (for BFGS-a-5.0e6-0.01, beststep = 2^5.0 and batchsize = prob.numdata)
 
-## By hand, let us try another single run of 3 hours with step = ... on SUSY
-# method_input = "SVRG";
+## ---> First gridsearch on SUSY-column-scaling of ... hours led us to fsol = ...
+## (for BFGS, beststep = ... and batchsize = prob.numdata)
+## (for BFGS-a ..., beststep = ... and batchsize = prob.numdata)
 
-# options = set_options(tol=10.0^(-16.0), skip_error_calculation=10^1, exacterror=false, max_iter=10^8, 
-#                       max_time=60.0*60.0*3.0, max_epocs=70, force_continue=true);
-# options.batchsize = 100;
-# options.stepsize_multiplier = 2.0; # beststep = 2.0 for batchsize = 100
 
-# ## Saving the optimization output in a JLD file
-# output = minimizeFunc(prob, method_input, options);
+# ## By hand, let us try another single run of 3 hours with step = ... on SUSY
+# # method_input = "SVRG";
 
-# ## Saving the optimization output in a JLD file
-# a, savename = get_saved_stepsize(prob.name, method_input, options);
-# a = nothing;
-# save("$(default_path)$(savename)_try_2.jld", "output", output)
+# # options = set_options(tol=10.0^(-16.0), skip_error_calculation=10^1, exacterror=false, max_iter=10^8, 
+# #                       max_time=60.0*60.0*3.0, max_epocs=70, force_continue=true);
+# # options.batchsize = 100;
+# # options.stepsize_multiplier = 2.0; # beststep = 2.0 for batchsize = 100
 
-# ## Setting the true solution as the smallest of both
-# prob.fsol = minimum(output.fs);
-# println("\n----------------------------------------------------------------------")
-# @printf "For %s, fsol = %f\n" prob.name prob.fsol
-# println("----------------------------------------------------------------------\n")
+# # ## Saving the optimization output in a JLD file
+# # output = minimizeFunc(prob, method_input, options);
 
-# ## Saving the solution in a JLD file
-# fsolfilename = get_fsol_filename(prob); # not coherent with get_saved_stepsize output
-# save("$(fsolfilename)_try_2.jld", "fsol", prob.fsol)
+# # ## Saving the optimization output in a JLD file
+# # a, savename = get_saved_stepsize(prob.name, method_input, options);
+# # a = nothing;
+# # save("$(default_path)$(savename)_try_2.jld", "output", output)
+
+# # ## Setting the true solution as the smallest of both
+# # prob.fsol = minimum(output.fs);
+# # println("\n----------------------------------------------------------------------")
+# # @printf "For %s, fsol = %f\n" prob.name prob.fsol
+# # println("----------------------------------------------------------------------\n")
+
+# # ## Saving the solution in a JLD file
+# # fsolfilename = get_fsol_filename(prob); # not coherent with get_saved_stepsize output
+# # save("$(fsolfilename)_try_2.jld", "fsol", prob.fsol)
 #endregion
 ######################################################################################################
