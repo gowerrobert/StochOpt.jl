accd5e620f109c6de7aa332c0a2559688b99404e
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2019-04-04 16:12:31 +0200

    parallel exp1_and_2 finished?

--- repeat_paper_experiments/repeat_optimal_minibatch_step_sizes_SAGA_paper_experiment_1_and_2_parallel.jl
+++ repeat_paper_experiments/repeat_optimal_minibatch_step_sizes_SAGA_paper_experiment_1_and_2_parallel.jl
@@ -5,7 +5,7 @@
 Goal: Computing the upper-bounds of the expected smoothness constant (exp. 1) and our step sizes estimates (exp. 2).
 
 ## --- THINGS TO CHANGE BEFORE RUNNING ---
-- line XXXXXXXXXXXX: enter your full path to the "StochOpt.jl/" repository in the *path* variable
+- line 36: enter your full path to the "StochOpt.jl/" repository in the *path* variable
 
 ## --- HOW TO RUN THE CODE ---
 To run only the first 2 problems (ridge regression for gauss-50-24-0.0_seed-1 with lambda=10^(-1) with and without scaling), open a terminal, go into the "StochOpt.jl/" repository and run the following command:
@@ -53,38 +53,39 @@ using Distributed
     pyplot() # No problem with pyplot when called in @everywhere statement
 end
 
-data_path = "$(path)data/";
 
 ## Experiments settings
-numsimu = 1; # number of runs of mini-batch SAGA for averaging the empirical complexity
 if all_problems
-    datasets = ["gauss-50-24-0.0_seed-1",
-                "diagints-24-0.0-100_seed-1",
-                "diagalone-24-0.0-100_seed-1",
-                "diagints-24-0.0-100-rotated_seed-1",
-                "diagalone-24-0.0-100-rotated_seed-1",
-                "slice",
-                "YearPredictionMSD_full",
-                "covtype_binary",
-                "rcv1_full",
-                "news20_binary",
-                "real-sim",
-                "ijcnn1_full"]
-    lambdas = [10^(-1), 10^(-3)]
-    num_problems = 42
+    problems = 1:42;
 else
-    datasets = ["gauss-50-24-0.0_seed-1"]
-    lambdas = [10^(-1)]
-    num_problems = 2
+    problems = 1:1;
 end
 
+datasets = collect(Iterators.flatten([fill("gauss-50-24-0.0_seed-1", 4),
+                                      fill("diagints-24-0.0-100_seed-1", 4),
+                                      fill("diagalone-24-0.0-100_seed-1", 4),
+                                      fill("diagints-24-0.0-100-rotated_seed-1", 4),
+                                      fill("diagalone-24-0.0-100-rotated_seed-1", 4),
+                                      fill("ijcnn1_full", 4),
+                                      fill("YearPredictionMSD_full", 4),
+                                      fill("covtype_binary", 4),
+                                      fill("slice", 4),
+                                      fill("real-sim", 2),
+                                      fill("rcv1_full", 2),
+                                      fill("news20_binary", 2)]))
+
+scalings = collect(Iterators.flatten([Iterators.flatten(fill(["none", "none", "column-scaling", "column-scaling"], 9)),
+                                      Iterators.flatten(fill(["none", "none"], 3))]))
+
+lambdas = collect(Iterators.flatten(fill([10^(-1), 10^(-3)], 21)))
+
+data_path = "$(path)data/";
+
 @time begin
-run_number = 1;
 @sync @distributed for idx_prob in problems
     data = datasets[idx_prob];
     scaling = scalings[idx_prob];
     lambda = lambdas[idx_prob];
-    skip_error = skip_errors[idx_prob];
     println("\n--- EXPERIMENT : ", idx_prob, " over ", length(problems), " ---\nInputs: ", data, " + ", scaling, " + ", lambda);
 
     Random.seed!(1);
@@ -96,13 +97,10 @@ run_number = 1;
 
     ## Setting up the problem
     println("\n--- Setting up the selected problem ---");
-    options = set_options(tol=precision, max_iter=10^8, max_epocs=600,
-                          max_time=60.0*60.0*5.0,
-                          skip_error_calculation=10^5,
-                          batchsize=1,
-                          regularizor_parameter = "normalized",
-                          initial_point="zeros", # is fixed not to add more randomness
-                          force_continue=true); # force continue if diverging or if tolerance reached
+    options = set_options(tol=10.0^(-1), max_iter=10^8, max_time=10.0^2, max_epocs=10^8,
+                                  regularizor_parameter = "normalized",
+                                  initial_point="zeros", # is fixed not to add more randomness
+                                  force_continue=false); # if true, forces continue if diverging or if tolerance reached
     u = unique(y);
     if length(u) < 2
         error("Wrong number of possible outputs");
@@ -118,36 +116,43 @@ run_number = 1;
     y = nothing;
 
     n = prob.numdata;
-    d = prob.numfeatures;
-    mu = prob.mu
-    Lmax = prob.Lmax;
-    L = prob.L;
-    # Lbar = prob.Lbar;
+    datathreshold = 24; # if n is too large we do not compute the exact expected smoothness constant nor corresponding step sizes
+
+    ########################### EMPIRICAL UPPER BOUNDS OF THE EXPECTED SMOOTHNESS CONSTANT ###########################
+    ## Computing the upper-bounds
+    expsmoothcst = nothing;
+    simple_bound, bernstein_bound, practical_approx, expsmoothcst = get_expected_smoothness_bounds(prob);
+
+    ### PLOTTING ###
+    println("\n--- Plotting upper-bounds ---");
+    pyplot()
+    plot_expected_smoothness_bounds(prob, simple_bound, bernstein_bound, practical_approx, expsmoothcst, showlegend=false);
+
+    ## Practical approximation equals true expected smoothness constant for b=1 and b=n as expected, but is not an upper-bound
+    if n <= datathreshold
+        println("\nPractical - Expected smoothness gap: ", practical_approx - expsmoothcst)
+        println("Simple - Practical gap: ", simple_bound - practical_approx)
+        println("Bernstein - Simple gap: ", bernstein_bound - simple_bound, "\n")
+    end
+    ##################################################################################################################
 
 
-    #=
-        TO FILL IN
-    =#
+    ##################################### EMPIRICAL UPPER BOUNDS OF THE STEP SIZES ####################################
+    ## Computing the step sizes
+    simple_step_size, bernstein_step_size, practical_step_size, hofmann_step_size, expsmooth_step_size = get_stepsize_bounds(prob, simple_bound, bernstein_bound, practical_approx, expsmoothcst);
 
+    ## Plotting
+    println("\n--- Plotting step sizes ---");
+    ## WARNING: there is still a problem of ticking non integer on the xaxis
+    pyplot()
+    plot_stepsize_bounds(prob, simple_step_size, bernstein_step_size, practical_step_size, hofmann_step_size, expsmooth_step_size, showlegend=false);
+    ##################################################################################################################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ########################################### SAVNG RESULTS ########################################################
+    println("\n--- Saving the bounds ---");
+    save_SAGA_nice_constants(prob, data, simple_bound, bernstein_bound, practical_approx, expsmoothcst,
+                             simple_step_size, bernstein_step_size, practical_step_size, expsmooth_step_size);
+    ##################################################################################################################
 
 end
 end
