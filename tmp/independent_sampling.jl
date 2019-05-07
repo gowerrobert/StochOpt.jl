2f3a39460708fcc00bf8cbbaa730f339b62c0e0f
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2019-05-07 10:43:21 +0200

    cleaning

--- tmp/independent_sampling.jl
+++ tmp/independent_sampling.jl
@@ -2,23 +2,6 @@
 
 using Distributions
 
-n = 5
-p = rand(n)
-D = [Binomial(1, i) for i in p]
-
-N = 1000
-V = [rand(d, N) for d in D]
-V = hcat(V...)'
-mean(V, dims=2)
-p
-
-## At each iteration perform
-n = 5
-b = 1
-p = (b/n)*ones(n) ## p_i = b/n for all i
-D = [Binomial(1, i) for i in p]
-indices = [rand(d) for d in D]
-
 """
     independent_sampling(probs)
 
@@ -46,6 +29,7 @@ function independent_sampling(probs::Array{Float64})
     return indices
 end
 
+## For b-independent sampling, at each descent step, do the following
 n = 1000
 b = 5
 probs = (b/n)*ones(n) ## p_i = b/n for all i
