ed8e75e735ae342c90432feaddee3779f59a5612
ngazagna <nidham.gazagnadou@telecom-paristech.fr>
2019-05-17 11:38:56 +0200

    Corrected optimal mini-batch size for Free and L-SVRG-D

--- src/SVRG_settings_and_util.jl
+++ src/SVRG_settings_and_util.jl
@@ -1,9 +1,10 @@
 """
-    optimal_minibatch_Free_SVRG_nice(prob::Prob, method, options::MyOptions)
+    optimal_minibatch_Free_SVRG_nice(m, n, mu, L, Lmax)
 
 Compute the optimal mini-batch size when the inner loop size m = n for the Free-SVRG algorithm with b-nice sampling.
 
 # INPUTS
+- **Int64** m: inner loop size
 - **Int64** n: number of data samples
 - **Float64** mu: strong convexity parameter of the objective function
 - **Float64** L: smoothness constant of the whole objective function f
@@ -11,55 +12,72 @@ Compute the optimal mini-batch size when the inner loop size m = n for the Free-
 # OUTPUTS
 - **Int64** minibatch_size: theoretical mini-batch size for Free-SVRG with b-nice sampling
 """
-function optimal_minibatch_Free_SVRG_nice(n, mu, L, Lmax)
-    b_hat = sqrt( (n*(3*Lmax-L)) / (2*(n*L-3*Lmax)) )
-    b_tilda = sqrt( (n*(3*Lmax-L)) / ((n*(n-1)*mu)/(2*log(2)) - n*L + 3*Lmax) ) # ERROR for one dataset : negative number
-    flag = "none"
-
-    if n < L/mu
-        if Lmax < n*L/3
-            minibatch_size = floor(Int, b_hat)
-            flag = "b_hat"
-        else
+function optimal_minibatch_Free_SVRG_nice(m, n, mu, L, Lmax)
+    if m == n
+        flag = "none"
+        if n < 3*L/mu
             minibatch_size = n
             flag = "n"
-        end
-    elseif L/mu <= n <= 3*Lmax/mu
-        if Lmax < n*L/3
+        elseif 3*L/mu <= n <= 3*Lmax/mu
+            b_hat = sqrt( ( n*(Lmax-L) ) / ( 2*(n*L-Lmax) ) )
+            b_tilda = ( 3*n*(Lmax-L) ) / ( mu*n*(n-1) - 3*(n*L-Lmax) )
+            if b_tilda < 1
+                error("b_tilda is non positive")
+            end
             minibatch_size = floor(Int, min(b_hat, b_tilda))
             flag = "min"
         else
-            minibatch_size = floor(Int, b_tilda)
-            flag = "b_tilda"
+            minibatch_size = 1
+            flag = "1"
         end
+        println(flag)
     else
-        minibatch_size = 1
-        flag = "1"
+        error("No optimal mini-bath size available for Free-SVRG with this inner loop size")
     end
-
-    println(flag)
 
     return minibatch_size
 end
 
-# """
-#     optimal_minibatch_L_SVRG_D_nice(prob::Prob, method, options::MyOptions)
+"""
+    optimal_minibatch_L_SVRG_D_nice(p, n, mu, L, Lmax)
 
-# Compute the optimal mini-batch size when the update probability is p = 1/n for the L-SVRG-D algorithm with b-nice sampling.
+Compute the optimal mini-batch size when the update probability is p = 1/n for the L-SVRG-D algorithm with b-nice sampling.
 
-# # INPUTS
-# - **Int64** n: number of data samples
-# - **Float64** mu: strong convexity parameter of the objective function
-# - **Float64** L: smoothness constant of the whole objective function f
-# - **Float64** Lmax: max of the smoothness constant of the f_i functions
-# # OUTPUTS
-# - **Int64** minibatch_size: theoretical mini-batch size for Free-SVRG with b-nice sampling
-# """
-# function optimal_minibatch_L_SVRG_D_nice(n, mu, L, Lmax)
-#
+# INPUTS
+- **Float64** p: update probability
+- **Int64** n: number of data samples
+- **Float64** mu: strong convexity parameter of the objective function
+- **Float64** L: smoothness constant of the whole objective function f
+- **Float64** Lmax: max of the smoothness constant of the f_i functions
+# OUTPUTS
+- **Int64** minibatch_size: theoretical mini-batch size for Free-SVRG with b-nice sampling
+"""
+function optimal_minibatch_L_SVRG_D_nice(p, n, mu, L, Lmax)
+    if norm(p - 1/n) < 1e-7
+        flag = "none"
+        ksi_p = ( (7-4*p)*(1-(1-p)^1.5) ) / ( p*(2-p)*(3-2*p) )
+        if n < 3*L/mu
+            minibatch_size = n
+            flag = "n"
+        elseif 3*L/mu <= n <= 3*Lmax/mu
+            b_hat = sqrt( ( n*(Lmax-L) ) / ( 2*(n*L-Lmax) ) )
+            b_tilda = ( 0.5*ksi_p*3*n*(Lmax-L) ) / ( mu*n*(n-1) - 0.5*ksi_p*3*(n*L-Lmax) )
+            if b_tilda < 1
+                error("b_tilda is non positive")
+            end
+            minibatch_size = floor(Int, min(b_hat, b_tilda))
+            flag = "min"
+        else
+            minibatch_size = 1
+            flag = "1"
+        end
+        println(flag)
+    else
+        error("No optimal mini-bath size available for L-SVRG-D with this probability")
+    end
 
-#     return minibatch_size
-# end
+    return minibatch_size
+end
 
 
 """
